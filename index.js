a382623d8cbfac7f70dc5eb7d65b6f7017bd1c7a
Alexey Novikov <velocityzen@gmail.com>
2013-08-28 00:39:26 +0400

    Fixed resources

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var apis = require('apis');
-var resource = apis.resources.res;
+var res = apis.resources.res;
 var handlers = apis.handlers;
 var data = handlers.data;
 var impl = handlers.impl;
@@ -13,42 +13,37 @@ var createImpl = function (ctrl, method) {
 	};
 };
 
-var addResources = function(contract, auth, resources, defMethods) {
-	var defaultMethods = defMethods || ['get', 'create', 'update', 'del'],
-		resRes = [];
+var add = function(contract, auth, resource, base) {
+	var methods = resource.method,
+		resourceMethods = {};
 
-	for(var i in resources) {
-		var r = resources[i],
-			ctrl = r[0],
-			methods = r[1] || undefined,
-			resourceMethods = {};
+	base = base || '';
 
-		if(methods === 'all' || methods === undefined) {
-			methods = defaultMethods;
-		}
+	for(var method in methods) {
+		var m = resourceMethods[method] = [];
 
-		for(var j in methods) {
-			var mName = methods[j],
-				m = resourceMethods[mName] = [];
-
-			if(!(mName === 'create' && ctrl.name === 'user')) {
-				if(mName === 'get') {
-					m.push(auth().opt); // get auth
-				} else {
-					m.push(auth()); // other methods auth
-				}
+		if(!(method === 'create' && resource.name === 'user')) {
+			if(method === 'get') {
+				m.push(auth().opt); // get auth
+			} else {
+				m.push(auth()); // other methods auth
 			}
-			m.push(data(ctrl.validators[mName]));
-			m.push(ret.any);
-			m.push(impl(createImpl(ctrl, mName) ));
 		}
-
-		resRes.push(
-			resource('/' + ctrl.name, resourceMethods)
-		);
+		m.push(data(methods[method]));
+		m.push(ret.any);
+		m.push(impl( createImpl(resource, method) ));
 	}
 
-	contract.addItems(resRes);
+	contract.add(res( base + '/' + resource.name, resourceMethods));
 };
 
-module.exports = addResources;
+var addItems = function(contract, auth, resources) {
+	for(var i in resources) {
+		add(contract, auth, resources[i]);
+	}
+};
+
+module.exports = {
+	add: add,
+	addItems: addItems
+};
